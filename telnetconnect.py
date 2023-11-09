5c2654f3088c8af4b6b854788af000a58173d3d7
Manraj2010 <145694339+Manraj2010@users.noreply.github.com>
2023-11-09 15:41:43 +0000

    Add files via upload

--- telnetconnect.py
+++ telnetconnect.py
@@ -1,110 +1,64 @@
-from netmiko import ConnectHandler
+import telnetlib
 import time
 
-# Define the device information
-device_info = {
-    'device_type': 'cisco_ios',
-    'ip': '192.168.56.101',
-    'username': input('Enter Username: '),  # The Username is "prne"
-    'password': input('Enter Password: '),  # The Password is "cisco123!"
-    'secret': 'class123!',  # Enable password
-    'timeout': 60,  # Set a longer timeout value (adjust as needed)
-}
+# Define variables
+ip_address = "192.168.56.101"
+username = input('Enter Username: ') #The username is "cisco"
+password = input('Enter Password: ') #The password is  "cisco123!"
+new_hostname = "R2"
+config_file = "running_config.txt"
 
-# Loopback interfaces and their IP addresses/subnet masks
-loopback_interfaces = {
-    'Loopback0': {'ip_address': '192.168.1.1', 'subnet_mask': '255.255.255.0'},
-    'Loopback1': {'ip_address': '192.168.1.2', 'subnet_mask': '255.255.255.0'},
-    'Loopback2': {'ip_address': '192.168.1.3', 'subnet_mask': '255.255.255.0'}
-}
+# Create a Telnet session
+tn = telnetlib.Telnet(ip_address)
 
-# Create a Netmiko SSH session
-ssh_session = ConnectHandler(**device_info)
+try:
+    # Read the initial prompt and wait for login
+    tn.read_until(b"Username: ", timeout=5)
+    tn.write(username.encode('ascii') + b"\n")
 
-# Enter enable mode
-ssh_session.enable()
+    # Wait for password prompt and enter the password
+    tn.read_until(b"Password: ", timeout=5)
+    tn.write(password.encode('ascii') + b"\n")
 
-# Introduce a delay for stability (optional)
-time.sleep(2)
-
-# Send a command to change the hostname
-new_hostname = 'R2'
-config_commands = [f'hostname {new_hostname}']
-ssh_session.send_config_set(config_commands)
-
-# Introduce a delay for stability (optional)
-time.sleep(2)
-
-# Configure loopback interfaces with IP addresses and subnet masks
-for interface, details in loopback_interfaces.items():
-    loopback_commands = [
-        f'interface {interface}',
-        f'ip address {details["ip_address"]} {details["subnet_mask"]}',
-    ]
-
-    ssh_session.send_config_set(loopback_commands)
+    # Waiting for the prompt and then a delay happens 
     time.sleep(2)
+    tn.read_very_eager()
 
-# Configure EIGRP to advertise the loopback interfaces
-eigrp_commands = [
-    'router eigrp 1',  # Assuming you are configuring EIGRP process ID 1
-]
+    # Send a command to configure the hostname
+    tn.write(b"configure terminal\n")
+    tn.read_until(b"#", timeout=5)
 
-for details in loopback_interfaces.values():
-    eigrp_commands.append(f'network {details["ip_address"]} 0.0.0.0')
+    tn.write(f"hostname {new_hostname}\n".encode('ascii'))
+    tn.read_until(b"#", timeout=5)
 
-ssh_session.send_config_set(eigrp_commands)
+    # Save the modified running configuration to the device
+    tn.write(b"end\n")
+    tn.read_until(b"#", timeout=5)
+    tn.write(b"write memory\n")
+    tn.read_until(b"#", timeout=5)
 
-# Introduce a delay for stability (optional)
-time.sleep(2)
+    # Read the running configuration
+    tn.write(b"show running-config\n")
+    running_config = tn.read_until(b"#", timeout=60).decode('ascii')
 
-# Configure RIP to advertise the loopback interfaces
-rip_commands = [
-    'router rip',
-]
+    # Save the running configuration to a local file
+    with open(config_file, "w") as f:
+        f.write(running_config)
 
-for details in loopback_interfaces.values():
-    rip_commands.append(f'network {details["ip_address"]}')
+    # Display success message
+    print("-------------------------------------------------")
+    print("        Telnet Configuration Script")
+    print("-------------------------------------------------")
+    print(f"Device IP: {ip_address}")
+    print(f"Username: {username}")
+    print("Password: ******")
+    print(f"Hostname changed to: {new_hostname}")
+    print(f"Running configuration saved to: {config_file}")
+    print("-------------------------------------------------")
 
-ssh_session.send_config_set(rip_commands)
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
 
-# Introduce a delay for stability (optional)
-time.sleep(2)
-
-# Configure OSPF to advertise the loopback interfaces
-ospf_commands = [
-    'router ospf 1',  # Assuming you are configuring OSPF process ID 1
-]
-
-for interface, details in loopback_interfaces.items():
-    ospf_commands.append(f'network {details["ip_address"]} 0.0.0.0 area 0')
-
-ssh_session.send_config_set(ospf_commands)
-
-# Introduce a delay for stability (optional)
-time.sleep(2)
-
-# Send a command to output the running configuration
-output = ssh_session.send_command('show running-config')
-
-# Save the running configuration to a file
-output_file = 'running_config.txt'
-with open(output_file, 'w') as config_file:
-    config_file.write(output)
-
-# Exit enable mode
-ssh_session.exit_enable_mode()
-
-# Disconnect from the device
-ssh_session.disconnect()
-
-# Display the information
-print('------------------------------------------------------')
-print('{:<20} {:<15} {:<15}'.format('Device IP', 'Username', 'Password'))
-print('{:<20} {:<15} {:<15}'.format(device_info['ip'], device_info['username'], device_info['password']))
-print('--- Running Configuration saved to:', output_file)
-print('--- Hostname changed to:', new_hostname)
-for interface, details in loopback_interfaces.items():
-    print(f'--- {interface} configured with IP address: {details["ip_address"]} and Subnet Mask: {details["subnet_mask"]}')
-print('--- EIGRP, RIP, and OSPF configurations applied for Loopback interfaces')
-print('------------------------------------------------------')
+finally:
+    # Close the Telnet session
+    tn.close()
